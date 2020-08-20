26b9b39910fb570b735105c2f0fe5f6874fee978
Milos Peric <steamestt@gmail.com>
2020-08-20 13:44:05 +0200

    +Add mock class

---
+++ Nedeljni_III_Milos_Peric/Nedeljni_III_Milos_Peric/ViewModel/ChooseIngredientsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedeljni_III_Milos_Peric.ViewModel
+{
+    class ChooseIngredientsViewModel
+    {
+    }
+}
